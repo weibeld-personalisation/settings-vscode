d0118e70f8d488e5ad42688a01e0186100d49f83
Daniel Weibel <danielmweibel@gmail.com>
2023-10-06 03:20:47 +0200

    Add backup and install scripts

---
+++ scripts/backup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Back up current VSCode settings and extensions to this repository. 
+
+[[ -d .git ]] || { echo "Error: must be run from repository root"; exit 1; }
+
+echo "Backing up settings.json"
+cp ~/Library/Application\ Support/Code/User/settings.json settings.json
+
+echo "Backing up extensions.json"
+cp ~/.vscode/extensions/extensions.json extensions.json
---
+++ scripts/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Install VSCode settings and extensions from this repository to the system.
+
+[[ -f settings.json && -f extensions.json ]] || { echo "Error: must be run from repository root"; exit 1; }
+
+target=~/Library/Application\ Support/Code/User/settings.json
+echo "Installing settings.json to $target"
+cp settings.json "$target"
+
+target=~/.vscode/extensions/extensions.json
+echo "Installing extensions.json to $target"
+cp extensions.json "$target"
