dc006fa976bef2c9e1f8ca3fe6fd3a5995d337a8
Daniel Weibel <danielmweibel@gmail.com>
2023-10-06 03:27:32 +0200

    Add comments to scripts

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -3,8 +3,10 @@
 
 [[ -d .git ]] || { echo "Error: must be run from repository root"; exit 1; }
 
+# macOS
 echo "Backing up settings.json"
 cp ~/Library/Application\ Support/Code/User/settings.json settings.json
 
+# macOS
 echo "Backing up extensions.json"
 cp ~/.vscode/extensions/extensions.json extensions.json
--- scripts/install.sh
+++ scripts/install.sh
@@ -3,10 +3,12 @@
 
 [[ -f settings.json && -f extensions.json ]] || { echo "Error: must be run from repository root"; exit 1; }
 
+# macOS
 target=~/Library/Application\ Support/Code/User/settings.json
 echo "Installing settings.json to $target"
 cp settings.json "$target"
 
+# macOS
 target=~/.vscode/extensions/extensions.json
 echo "Installing extensions.json to $target"
 cp extensions.json "$target"
